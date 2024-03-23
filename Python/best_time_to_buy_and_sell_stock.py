570730dce14da920c036c6d54d69f543e9cb519f
jyztintan <justintanwk2001@gmail.com>
2024-03-23 21:10:48 +0800

    Create best_time_to_buy_and_sell_stock.py
    Leetcode problem #121

---
+++ Python/best_time_to_buy_and_sell_stock.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxProfit(self, prices) -> int:
+        buy = float('inf')
+        profit = 0
+        for price in prices:
+            if price < buy:
+                buy = price
+            elif price - buy > profit:
+                profit = price - buy
+        return profit
+
+test = Solution()
+print(test.maxProfit([7,1,5,3,6,4]))
+print(test.maxProfit([7,6,4,3,1]))
