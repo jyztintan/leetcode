9d9c9e5366715732a2119508372efb10710802f0
jyztintan <justintanwk2001@gmail.com>
2024-06-11 22:36:25 +0800

    Add Trapping Rain Water

--- Python/trapping_rain_water.py
+++ Python/trapping_rain_water.py
@@ -1,25 +1,42 @@
-def trap(heights) -> int:
-    max_left = []
-    max_right = []
-    highest_left = 0
-    for num in heights:
-        max_left.append(highest_left)
-        if num > highest_left:
-            highest_left = num
-    highest_right = 0
-    for num in reversed(heights):
-        max_right.append(highest_right)
-        if num > highest_right:
-            highest_right = num
-    max_right.reverse()
-    total = 0
-    for i in range(len(heights)):
-        if height[i] < min(max_left[i], max_right[i]):
-            total += min(max_left[i], max_right[i]) - height[i]
-    return total
+class Solution(object):
+    def trap(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+
+        # Create the 2 pointers, and keep track of highest height from left and right
+        l, max_left = 0, height[0]
+        r = len(height) - 1
+        max_right = height[r]
+        total = 0
+
+        # While the 2 pointers converge, we always keep the higher one to make sure our ans is optimised
+        while l < r:
+
+            # Right is higher than left, so keep the right and move the left up hoping for a higher one
+            if max_left < max_right:
+                l += 1
+
+                # For this new column, if it is lower than the previous highest, then we add the volume water trapped
+                # which is just the difference. Otherwise, it is the new highest and no water trapped.
+                # We can be sure the water is trapped since the right highest is guaranteed to be higher than left
+                total += max(0, (max_left - height[l]))
+                max_left = max(max_left, height[l])
+
+            # Left higher than right, so keep the left and move right down hoping for a higher one
+            else:
+                r -= 1
+
+                # For this new column, if it is lower than the previous highest, then we add the volume water trapped
+                # which is just the difference. Otherwise, it is the new highest and no water trapped.
+                # We can be sure the water is trapped since the left highest is guaranteed to be higher than right
+                total += max(0, max_right - height[r])
+                max_right = max(max_right, height[r])
+        return total
 
 
-height = [0,1,0,2,1,0,1,3,2,1,2,1]
-print(trap(height))
-height = [4,2,0,3,2,5]
-print(trap(height))
+# height = [0,1,0,2,1,0,1,3,2,1,2,1]
+# print(trap(height))
+# height = [4,2,0,3,2,5]
+# print(trap(height))
