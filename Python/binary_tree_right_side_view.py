792f3a1daf25809406a75e31c4f1a6d22b8e7586
jyztintan <justintanwk2001@gmail.com>
2024-06-15 10:02:50 +0800

    Add Binary Tree Right Side View (Fix #4)

---
+++ Python/binary_tree_right_side_view.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution(object):
+    def rightSideView(self, root):
+        def check_by_height(root, height):
+            if not root:
+                return
+            if len(res) == height:
+                res.append(root.val)
+            check_by_height(root.right, height + 1)
+            check_by_height(root.left, height + 1)
+            return
+
+        res = []
+        check_by_height(root, 0)
+        return res
+
