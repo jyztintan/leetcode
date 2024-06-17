6b6c97e695d5ab710d872e77042f88c868becac4
jyztintan <justintanwk2001@gmail.com>
2024-06-17 10:31:26 +0800

    Refine Car Fleet

--- Python/car_fleet.py
+++ Python/car_fleet.py
@@ -1,37 +1,30 @@
-def car_fleet(target, position, speed):
-    cars = []
-    for i in range(len(speed)):
-        cars.append([position[i], speed[i]])
-    cars.sort(key = lambda x:x[0], reverse = True)
-    timing = list(map(lambda car: (target - car[0])/car[1], cars))
-    count, max_time = 1, timing[0]
-    for time in timing:
-        if time <= max_time:
-            continue
-        count += 1
-        max_time = time
-    return count
-
-# Recommended tutorial from NeetCode but my solution is faster :D
-# def car_fleet(target, position, speed):
-#     pairs = [[pos, spd] for pos, spd in zip(position, speed)]
-#     stack = []
-#     for pair in sorted(pairs)[::-1]:
-#         stack.append((target - pair[0])/pair[1])
-#         if len(stack) >= 2 and stack[-1] <= stack[-2]:
-#             stack.pop()
-#     return len(stack)
+class Solution(object):
+    def carFleet(self, target, position, speed):
+        cars = [(pos, sp) for pos, sp in zip(position, speed)]
+        cars.sort(key=lambda x: x[0], reverse=True)
+        fleets = [(target - car[0])/car[1] for car in cars]
+        ans, max_time = 0, -1
+        for fleet in fleets:
+            if fleet > max_time:
+                ans += 1
+                max_time = fleet
+        return ans
 
 
-target = 12
-position = [10,8,0,5,3] 
-speed = [2,4,1,1,3]
-print(car_fleet(target, position, speed))
-target = 10
-position = [8,3,7,4,6,5]
-speed = [4,4,4,4,4,4]
-print(car_fleet(target, position, speed))
-target = 100
-position = [0,2,4]
-speed = [4,2,1]
-print(car_fleet(target, position, speed))
+
+# sol = Solution()
+# position = [6, 8]
+# speed = [3, 2]
+# print(sol.carFleet(10, position, speed))
+# target = 12
+# position = [10,8,0,5,3]
+# speed = [2,4,1,1,3]
+# print(sol.carFleet(target, position, speed))
+# target = 10
+# position = [8,3,7,4,6,5]
+# speed = [4,4,4,4,4,4]
+# print(sol.carFleet(target, position, speed))
+# target = 100
+# position = [0,2,4]
+# speed = [4,2,1]
+# print(sol.carFleet(target, position, speed))
