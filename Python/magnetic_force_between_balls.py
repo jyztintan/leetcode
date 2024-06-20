4da8e4765512e9c2c9df68133d1d7dbfe9132d14
jyztintan <justintanwk2001@gmail.com>
2024-06-20 10:58:11 +0800

    Update magnetic_force_between_balls.py

--- Python/magnetic_force_between_balls.py
+++ Python/magnetic_force_between_balls.py
@@ -36,7 +36,7 @@ class Solution:
 
         return best
 
-position = [1,2,3,4,5,6,7,8,9,10]
-print(Solution().maxDistance(position, 4))
-position = [5,4,3,2,1,1000000000]
-print(Solution().maxDistance(position, 2))
+# position = [1,2,3,4,5,6,7,8,9,10]
+# print(Solution().maxDistance(position, 4))
+# position = [5,4,3,2,1,1000000000]
+# print(Solution().maxDistance(position, 2))
