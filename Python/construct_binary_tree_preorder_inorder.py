698ea857f5b1c945c1d6582083f84fcd2102d856
jyztintan <justintanwk2001@gmail.com>
2024-07-05 00:02:30 +0800

    Add Construct Binary Tree Preorder and Inorder

---
+++ Python/construct_binary_tree_preorder_inorder.py
@@ -0,0 +1,16 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def buildTree(self, preorder, inorder):
+        if not preorder:
+            return None
+        root_val = preorder[0]
+        root = TreeNode(root_val)
+        idx = inorder.index(root_val)
+        root.left = self.buildTree(preorder[1:idx + 1], inorder[:idx])
+        root.right = self.buildTree(preorder[idx + 1:], inorder[idx + 1:])
+        return root
