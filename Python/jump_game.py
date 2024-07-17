49948c93e0bc53d1cdda7cd35c3d63d2d77e88f2
jyztintan <justintanwk2001@gmail.com>
2024-07-17 16:34:05 +0800

    Add Jump Game

---
+++ Python/jump_game.py
@@ -0,0 +1,37 @@
+# Solution 1: Using Greedy array modification?
+class Solution:
+
+    def canJump(self, nums) -> bool:
+        target = len(nums) - 1
+
+        for i in range(target, -1, -1):
+            # We can guarantee that the target can be reached if we reach this index.
+            # Hence, we can set this index as the new target
+            if nums[i] + i >= target:
+                target = i
+        # Since we have iteratively modified the goal, if it reaches 0, it means that we are guaranteed
+        # to be able to reach the actual end goal
+        return target == 0
+
+
+# Using Stack and DFS Traversal
+# class Solution:
+#
+#     def canJump(self, nums) -> bool:
+#         target = len(nums) - 1
+#
+#         st = []
+#         visited = set()
+#         st.append(0)
+#         while st:
+#             curr = st.pop()
+#             for i in range(nums[curr] + 1):
+#                 if i + curr == target:
+#                     return True
+#                 if i + curr not in visited:
+#                     st.append(i + curr)
+#                     visited.add(i + curr)
+#         return False
+
+# nums = [3,2,1,0,4]
+# print(Solution().canJump(nums))
