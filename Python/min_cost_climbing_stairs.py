a5c99575a443c00c679af5bd24d047a949f091c3
jyztintan <justintanwk2001@gmail.com>
2024-07-19 20:41:52 +0800

    Add Min Cost Climbing Stairs

--- Python/min_cost_climbing_stairs.py
+++ Python/min_cost_climbing_stairs.py
@@ -1,13 +1,22 @@
 class Solution:
     def minCostClimbingStairs(self, cost) -> int:
-        if len(cost) <= 2:
-            return min(cost)
+        # memo = {len(cost) : 0, len(cost) - 1 : cost[-1]}
+        #
+        # def min_cost(step):
+        #     if step in memo:
+        #         return memo[step]
+        #     memo[step] = cost[step] + min(min_cost(step + 1), min_cost(step + 2))
+        #
+        # return min(min_cost(0), min_cost(1))
 
-        return min(self.minCostClimbingStairs(cost[1:],) ,  self.minCostClimbingStairs(cost[1:],)
+        for i in range(len(cost) - 3, -1, -1):
+            cost[i] += min(cost[i + 1], cost[i + 2])
 
-cost = [10,15,20]
-print(Solution().minCostClimbingStairs(cost))
-cost = [1,100,1,1,1,100,1,1,100,1]
-print(Solution().minCostClimbingStairs(cost))
-cost = [0, 2, 2, 1]
-print(Solution().minCostClimbingStairs(cost))
+        return min(cost[0], cost[1])
+
+# cost = [10,15,20]
+# print(Solution().minCostClimbingStairs(cost))
+# cost = [1,100,1,1,1,100,1,1,100,1]
+# print(Solution().minCostClimbingStairs(cost))
+# cost = [0, 2, 2, 1]
+# print(Solution().minCostClimbingStairs(cost))
