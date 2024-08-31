b4078112cb3b9d33ca52a3a68330ddd3e92f1b5a
jyztintan <justintanwk2001@gmail.com>
2024-08-31 09:04:54 +0800

    Refine Path with Maximum Probability

--- Python/path_with_max_prob.py
+++ Python/path_with_max_prob.py
@@ -2,38 +2,30 @@ import heapq
 from typing import List
 
 
+
 class Solution:
     def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start_node: int,
                        end_node: int) -> float:
 
-        adj_list = {}
-        for i in range(len(edges)):
-            u,v = edges[i]
-            prob = succProb[i]
-            if u not in adj_list:
-                adj_list[u] = []
-            adj_list[u].append((v, prob))
-            if v not in adj_list:
-                adj_list[v] = []
-            adj_list[v].append((u, prob))
+        adj_list = {i: [] for i in range(n)}
+        for i, (u, v) in enumerate(edges):
+            adj_list[u].append((v, succProb[i]))
+            adj_list[v].append((u, succProb[i]))
 
         max_heap = [(-1, start_node)]
+
         visited = set()
-
         while max_heap:
-            prob, curr = heapq.heappop(max_heap)
-            visited.add(curr)
-            if curr == end_node:
-                return -prob
-
-            if curr not in adj_list:
+            prob, node = heapq.heappop(max_heap)
+            if node in visited:
                 continue
-            for neighbour, next_prob in adj_list[curr]:
-                if neighbour in visited:
-                    continue
-                heapq.heappush(max_heap, (next_prob * prob, neighbour))
+            visited.add(node)
+            if node == end_node:
+                return -prob
+            for neighbour, weight in adj_list[node]:
+                heapq.heappush(max_heap, (weight * prob, neighbour))
 
         return 0
 
-# edges = [[0,1],[1,2],[0,2]]
-# print(Solution().maxProbability(3, edges, [0.5,0.5,0.2], 0, 2))
+edges = [[0,1],[1,2],[0,2]]
+print(Solution().maxProbability(3, edges, [0.5,0.5,0.2], 0, 2))
