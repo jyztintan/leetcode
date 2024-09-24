5a069a441a12aff91c673894467af9f638f85af4
jyztintan <justintanwk2001@gmail.com>
2024-09-24 09:51:52 +0800

    Add Find Length Longest Common Prefix

--- Python/len_longest_common_prefix.py
+++ Python/len_longest_common_prefix.py
@@ -1,3 +1,4 @@
+# Solution 1: Prefix HashSet
 class Solution:
     def longestCommonPrefix(self, arr1, arr2) -> int:
         arr1 = list(map(str, arr1))
@@ -10,7 +11,32 @@ class Solution:
         for num in arr2:
             for i in range(len(num)):
                 if num[:i + 1] in possible:
-                    longest = max(longest, len(num) - i)
+                    longest = max(longest, i + 1)
+        return longest
+
+# Solution 2: Trie
+class Solution:
+    def longestCommonPrefix(self, arr1, arr2) -> int:
+        arr1 = list(map(str, arr1))
+        arr2 = list(map(str, arr2))
+        trie = {}
+        for num in arr1:
+            traverse = trie
+            for c in num:
+                if c not in traverse:
+                    traverse[c] = {}
+                traverse = traverse[c]
+
+        longest = 0
+        for num in arr2:
+            traverse = trie
+            depth = 0
+            for c in num:
+                if c not in traverse:
+                    break
+                traverse = traverse[c]
+                depth += 1
+            longest = max(longest, depth)
         return longest
 
 
