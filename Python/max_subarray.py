16567dd6c3a47000dbf4f18846e4a36119e40f8f
jyztintan <justintanwk2001@gmail.com>
2024-10-31 15:17:51 +0800

    Refine Max Subarrays

--- Python/max_subarray.py
+++ Python/max_subarray.py
@@ -2,31 +2,11 @@
 
 class Solution:
     def maxSubArray(self, nums) -> int:
-        if len(nums) == 1:
-            return nums[0]
-        mid = len(nums)//2
-        left_sum = self.maxSubArray(nums[:mid])
-        right_sum = self.maxSubArray(nums[mid:])
-        mid_sum = self.findSum(nums)
-        return max(left_sum, right_sum, mid_sum)
+        curr_subarr = -float('inf')
+        max_subarr = -float('inf')
 
-    def findSum(self, nums):
-        if len(nums) == 1:
-            return nums[0]
-        max_l = -float("inf")
-        i = len(nums)//2 - 1
-        sum = 0
-        while i >= 0:
-            sum += nums[i]
-            if sum > max_l:
-                max_l = sum
-            i -= 1
-        max_r = -float("inf")
-        i = len(nums)//2
-        sum = 0
-        while i < len(nums):
-            sum += nums[i]
-            if sum > max_r:
-                max_r = sum
-            i += 1
-        return max_l + max_r
+        for num in nums:
+            curr_subarr = max(num, curr_subarr + num)
+            max_subarr = max(max_subarr, curr_subarr)
+
+        return max_subarr
