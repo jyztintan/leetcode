66755de4f0cf6e8087cc6b7b549573abc3a49fcd
jyztintan <justintanwk2001@gmail.com>
2024-10-31 20:10:09 +0800

    Add Shortest Path Obstacles Elimination

--- Python/shortest_path_obstacle_elimination.py
+++ Python/shortest_path_obstacle_elimination.py
@@ -6,31 +6,32 @@ class Solution:
     memoized = {}
     def shortestPath(self, grid: List[List[int]], k: int) -> int:
         m, n = len(grid), len(grid[0])
-        shortest = {}
+
+        # If we have enough limit, we can just break all obstacles through the shortest path
+        if k >= m + n - 2:
+            return m + n - 2
+
+        curr = (0, 0, k)
         q = Queue()
-        q.put((0, 0, 0, 0))
+        q.put((0, curr))
+        seen = {curr}
         directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
         while not q.empty():
-            row, col, encountered, path = q.get()
-            if encountered <= k and path < shortest.get((row, col, encountered), float('inf')):
-                shortest[(row, col, encountered)] = path
+            steps, (row, col, k) = q.get()
+            if row == m - 1 and col == n - 1:
+                return steps
 
-                path += 1
-                for x, y in directions:
-                    new_row, new_col = row + x, col + y
-                    if 0 <= new_row < m and 0 <= new_col < n:
-                        if grid[new_row][new_col] == 0:
-                            q.put((new_row, new_col, encountered, path))
-                        elif encountered + 1 <= k:
-                            q.put((new_row, new_col, encountered + 1, path))
-        ans = float('inf')
-        for i in range(k + 1):
-            if (m - 1, n - 1, i) in shortest:
-                ans = min(ans, shortest[(m - 1, n - 1, i)])
-        return ans if ans != float('inf') else -1
+            for x, y in directions:
+                new_row, new_col = row + x, col + y
+                if (0 <= new_row < m) and (0 <= new_col < n):
+                    limit = k - grid[new_row][new_col]
+                    curr = (new_row, new_col, limit)
+                    if k >= 0 and curr not in seen:
+                        seen.add(curr)
+                        q.put((steps + 1, curr))
+        return -1
 
-# grid = [[0,0,0],[1,1,0],[0,0,0],[0,1,1],[0,0,0]]
-# grid = [[0,1,1],[1,1,1],[1,0,0]]
-# grid = [[0]]
-# print(Solution().shortestPath(grid, 1))
+
+grid = [[0,0,0],[1,1,0],[0,0,0],[0,1,1],[0,0,0]]
+print(Solution().shortestPath(grid, 1))
 
