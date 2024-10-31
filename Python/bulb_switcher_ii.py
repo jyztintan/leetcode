e913e552bd2506b4ff78a04c796860b2fdc5221a
jyztintan <justintanwk2001@gmail.com>
2024-10-31 20:25:24 +0800

    Add Bulb Switcher II

--- Python/bulb_switcher_ii.py
+++ Python/bulb_switcher_ii.py
@@ -1,2 +1,18 @@
 class Solution:
     def flipLights(self, n: int, presses: int) -> int:
+
+        # The first 3 bulbs uniquely define the entire sequence
+        n = min(3, n)
+
+        # No presses allowed so only one state
+        if presses == 0:
+            return 1
+
+        # 1 press allowed, possibly 2, 3 or 4 states depending on n
+        if presses == 1:
+            return [2, 3, 4][n - 1]
+        # 2 presses allowed, possibly 2, 4 or 7 states depending on n
+        elif presses == 2:
+            return [2, 4, 7][n - 1]
+        # More than 2 presses allowed, possibly 2, 4 or 8 states depending on n
+        return [2, 4, 8][n - 1]
