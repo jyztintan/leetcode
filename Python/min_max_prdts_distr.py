0c02c5139c5b8c881f9c0d6cd7df3e6030f4913d
jyztintan <justintanwk2001@gmail.com>
2024-11-15 09:53:56 +0800

    Add Minimized Maximum of Products Distributed

---
+++ Python/min_max_prdts_distr.py
@@ -0,0 +1,29 @@
+import math
+
+
+class Solution:
+    def minimizedMaximum(self, n: int, quantities) -> int:
+
+        # Number of product types
+        m = len(quantities)
+        total = sum(quantities)
+
+        # Try different allocations of stores
+        def distribute(k):
+            print(list(math.ceil(q / k) for q in quantities))
+            return sum(math.ceil(q / k) for q in quantities) <= n
+
+        # The number of products distributed is [0, total quantity]
+        low = 0
+        high = total
+        best = 0
+        while low <= high:
+            mid = (low + high)//2
+            if distribute(mid):
+                high = mid - 1
+                best = mid
+            else:
+                low = mid + 1
+
+        return best
+
