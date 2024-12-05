56b4d7660a835532b935e5f71d3183cef5e647e4
jyztintan <justintanwk2001@gmail.com>
2024-12-05 12:47:06 +0800

    Refine Encode decode strings

--- Python/encode_decode_strings.py
+++ Python/encode_decode_strings.py
@@ -1,21 +1,52 @@
-class Solution:
+# If we are allowed to use non-ascii characters then simply use any as a delimiter
+# Time O(N): just need to add/split delimiter between the words
+# Space O(N + K): we would get an output of N characters plus K instances for delimiters
+# where N and K are the total number of characters and words in strs.
+class Codec:
+    def encode(self, strs) -> str:
+        """Encodes a list of strings to a single string.
+        """
+        encoded = ''
+        for word in strs:
+            encoded += word.replace('/', '//') + '/$'
+        return encoded
 
-    def encode(self, strs):
-        res = ""
-        for s in strs:
-            res += len(s) + "#" + s
-        return res
-    def decode(self, s):
-        index = 0
-        ans = []
-        while index < len(s):
-            pos = s.find("#", index)
-            if pos == -1:
-                return ans
-            num = int(s[index:pos])
-            ans.append(s[pos+1:pos+num+1])
-            index = pos + num + 1
-        return ans
+    def decode(self, s: str):
+        """Decodes a single string to a list of strings.
+        """
+        decoded = []
+        ptr = 0
+        curr = ''
+        while ptr < len(s):
+            if s[ptr:ptr + 2] == '/$':
+                decoded.append(curr)
+                curr = ""
+                ptr += 2
+            elif s[ptr:ptr + 2] == "//":
+                curr += '/'
+                ptr += 2
+            else:
+                curr += s[ptr]
+                ptr += 1
+        return decoded
 
-# s = Solution()
-# print(s.decode("3#hat4#thor"))
+
+# If we are allowed to use non-ascii characters then simply use any as a delimiter
+# Time O(N): just need to add/split delimiter between the words
+# Space O(N + K): we would get an output of N characters plus K instances for delimiters
+# where N and K are the total number of characters and words in strs.
+class Codec:
+    def encode(self, strs: List[str]) -> str:
+        """Encodes a list of strings to a single string.
+        """
+        return 'π'.join(strs)
+
+    def decode(self, s: str) -> List[str]:
+        """Decodes a single string to a list of strings.
+        """
+        return s.split('π')
+
+
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.decode(codec.encode(strs))
