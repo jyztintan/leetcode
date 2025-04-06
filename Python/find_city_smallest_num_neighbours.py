f6dbd43d4f44b0b7aa4e1ec26df51cbbe5cb6c0e
jyztintan <justintanwk2001@gmail.com>
2025-04-06 20:48:28 +0200

    Refine

--- Python/find_city_smallest_num_neighbours.py
+++ Python/find_city_smallest_num_neighbours.py
@@ -1,51 +1,35 @@
 # This solution seems like it can be better optimised. Time complexity: O(V^2logV)?
 
-import heapq
+from heapq import *
 from collections import defaultdict
 from typing import List
+
+
 class Solution:
     def findTheCity(self, n: int, edges: List[List[int]], distanceThreshold: int) -> int:
-
-        # Instantiate the adjacency list for easy access to edges
         adj_list = defaultdict(set)
         for u, v, w in edges:
             adj_list[u].add((v, w))
             adj_list[v].add((u, w))
 
-        # We perform a modified version of Dijkstra's algorithm that terminates when the distanceThreshold is reached
-        def dijkstra(src):
-            neighbours = [float('inf')] * n
-            pq = []
-            heapq.heappush(pq, (0, src))
-            # We exclude (negate) the source node itself
-            ans = -1
+        def dijkstra(node):
+            distances = [float('inf')] * n
+            distances[node] = 0
+            pq = [(0, node)]
             while pq:
-                dist, u = heapq.heappop(pq)
-                # If this node has been visited, we skip processing it again
-                if neighbours[u] != float('inf'):
-                    continue
-                # Since our pq processes distances increasingly, all subsequent edges from here on would exceed
-                # the distance threshold, and hence would be irrelevant
+                dist, u = heappop(pq)
                 if dist > distanceThreshold:
                     break
-                neighbours[u] = dist
-                ans += 1
-                # Add edges of the newly visited vertex into the pq
-                for v, w in adj_list[u]:
-                    heapq.heappush(pq, (dist + w, v))
+                for neighbour, weight in adj_list[u]:
+                    if distances[neighbour] > dist + weight:
+                        distances[neighbour] = dist + weight
+                        heappush(pq, (dist + weight, neighbour))
+            return distances
 
-            return ans
-
-        # We perform Dijkstra's algorithm for each node as source, and keep track of the city with the least neighbours
-        least_neighbours = float('inf')
-        city = -1
-        for src in range(n):
-            neighbours = dijkstra(src)
-            if neighbours <= least_neighbours:
-                least_neighbours = neighbours
-                city = src
-
+        neighbours, city = float('inf'), -1
+        for node in range(n):
+            distances = dijkstra(node)
+            count = sum(int(distance <= distanceThreshold) for distance in distances)
+            if count <= neighbours:
+                neighbours, city = count, node
         return city
-
-# edges = [[0,1,3],[1,2,1],[1,3,4],[2,3,1]]
-# print(Solution().findTheCity(4, edges, 4))
