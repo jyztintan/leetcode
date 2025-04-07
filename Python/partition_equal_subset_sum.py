6cf419456ce7dcdd5a45a505ccc9fdcd86f13b3f
jyztintan <justintanwk2001@gmail.com>
2025-04-07 08:54:50 +0200

    Refine

--- Python/partition_equal_subset_sum.py
+++ Python/partition_equal_subset_sum.py
@@ -1,27 +1,20 @@
 # Using sets to avoid recomputation O(N * M) since the possible set may calculate all possible M = sum(nums)/2
 class Solution:
     def canPartition(self, nums) -> bool:
+        total = sum(nums)
 
         # If the total is not divisible by 2, then it obviously can't be partitioned
-        total = sum(nums)
         if total % 2:
             return False
 
-        # The target sum should be the total divided by 2
-        target = total / 2
-
-        # The idea is that we use sets so that we don't have to compute same values
-        possible = set()
-        possible.add(0)
+        target = total // 2
+        possible = {target}
         for num in nums:
-            # If the negation of this number is inside, we can confidently say we found the partition
-            if target - num in possible:
+            if num in possible:
                 return True
-            # For each number, we can either choose to include or exclude in the component of the partition
-            to_add = set()
-            for prev in possible:
-                to_add.add(num + prev)
-            possible.update(to_add)
+            # Choose to include or exclude in the new subset
+            for subset in possible.copy():
+                possible.add(subset - num)
         return False
 
 
