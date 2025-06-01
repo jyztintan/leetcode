5814bc7f57f7a392cf36ca0771d84e373cdac6fc
jyztintan <justintanwk2001@gmail.com>
2025-06-01 10:53:25 +0800

    Add Distribute Candies Among Children II

---
+++ Python/distribute_candies_children_ii.py
@@ -0,0 +1,7 @@
+class Solution:
+    def distributeCandies(self, n: int, limit: int) -> int:
+        count = 0
+        for i in range(min(limit, n) + 1):
+            if n - i <= 2 * limit:
+                count += min(n - i, limit) - max(0, n - i - limit) + 1
+        return count
