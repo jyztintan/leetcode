381b9447554506634cb7eda34ebea618edc316a9
jyztintan <justintanwk2001@gmail.com>
2025-06-23 21:49:18 +0800

    Add Happy Number

---
+++ Python/happy_num.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        seen = set()
+        while n != 1:
+            if n in seen:
+                return False
+            seen.add(n)
+            new = 0
+            for c in str(n):
+                new += int(c) ** 2
+            n = new
+        return True
--- Python/reverse_int.py
+++ Python/reverse_int.py
@@ -1,9 +1,9 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        if x < 0:
-            new = - int(str(x)[:0:-1])
-        else:
-            new = int(str(x)[::-1])
-        if new < -2 ** 31 or new > 2 ** 31 - 1:
+        sign = 1 if x >= 0 else -1
+        string_num = str(abs(x))
+        num = int(string_num[::-1]) * sign
+        if num > 2 ** 31 -1 or num < - 2 ** 31:
             return 0
-        return new
+        return num
+    
