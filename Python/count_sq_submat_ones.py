e155934caacdbcf5b1608197585193d65b3d3b06
jyztintan <justintanwk2001@gmail.com>
2025-08-21 13:50:25 +0800

    Add new pset

--- Python/count_sq_submat_ones.py
+++ Python/count_sq_submat_ones.py
@@ -1,19 +1,55 @@
+# Bottom up solution with space optimisation
 class Solution:
-    def countSquares(self, matrix) -> int:
-        m = len(matrix)
-        n = len(matrix[0])
-        memoize = {}
+    def countSquares(self, mat: List[List[int]]) -> int:
+        m, n = len(mat), len(mat[0])
+        dp = [0] * n
+        ans = 0
+        for i in range(m):
+            new_dp = [0] * n
+            for j in range(n):
+                if mat[i][j] == 1:
+                    if i == 0 or j == 0:
+                        new_dp[j] = 1
+                    else:
+                        new_dp[j] = 1 + min(dp[j], new_dp[j-1], dp[j-1])
+                    ans += new_dp[j]
+            dp = new_dp
+        return ans
 
-        def count(row, col):
-            if row == m or col == n or not matrix[row][col]:
+# Bottom up solution treat the top left as the bottom right
+class Solution:
+    def countSquares(self, mat: List[List[int]]) -> int:
+        m, n = len(mat), len(mat[0])
+        dp = [[0] * n for _ in range(m)]
+        ans = 0
+        for i in range(m):
+            for j in range(n):
+                if mat[i][j] == 1:
+                    if i == 0 or j == 0:
+                        dp[i][j] = 1
+                    else:
+                        dp[i][j] = 1 + min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1])
+                    ans += dp[i][j]
+        return ans
+
+
+# Top down solution
+class Solution:
+    def countSquares(self, mat: List[List[int]]) -> int:
+        m, n = len(mat), len(mat[0])
+        dp = [[-1] * n for _ in range(m)]
+
+        def find(row, col):
+            if row == m or col == n or mat[row][col] == 0:
                 return 0
-            if (row, col) in memoize:
-                return memoize[(row, col)]
-            memoize[(row, col)] = 1 + min(count(row + 1, col), count(row + 1, col + 1), count(row, col + 1))
-            return memoize[(row, col)]
+            if dp[row][col] != -1:
+                return dp[row][col]
+            dp[row][col] = 1 + min(find(row + 1, col + 1), find(row + 1, col), find(row, col + 1))
+            return dp[row][col]
 
         ans = 0
         for row in range(m):
             for col in range(n):
-                ans += count(row, col)
+                ans += find(row, col)
         return ans
+
