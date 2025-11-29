32e9bab207d2fc78f38ea5cd874a051474a53cf7
jyztintan <justintanwk2001@gmail.com>
2025-11-29 10:46:08 +0800

    Add new pset

---
+++ Python/nested_lst_weight_sum_ii.py
@@ -0,0 +1,66 @@
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+# class NestedInteger:
+#    def __init__(self, value=None):
+#        """
+#        If value is not specified, initializes an empty list.
+#        Otherwise initializes a single integer equal to value.
+#        """
+#
+#    def isInteger(self):
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        :rtype bool
+#        """
+#
+#    def add(self, elem):
+#        """
+#        Set this NestedInteger to hold a nested list and adds a nested integer elem to it.
+#        :rtype void
+#        """
+#
+#    def setInteger(self, value):
+#        """
+#        Set this NestedInteger to hold a single integer equal to value.
+#        :rtype void
+#        """
+#
+#    def getInteger(self):
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        Return None if this NestedInteger holds a nested list
+#        :rtype int
+#        """
+#
+#    def getList(self):
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        Return None if this NestedInteger holds a single integer
+#        :rtype List[NestedInteger]
+#        """
+
+class Solution:
+    def depthSumInverse(self, nestedList: List[NestedInteger]) -> int:
+        integers_depth = []
+        max_depth = [0]
+        q = []
+
+        def traverse(curr, depth):
+            if curr.isInteger():
+                integers_depth.append((curr.getInteger(), depth))
+                max_depth[0] = max(max_depth[0], depth)
+                return
+            lst = curr.getList()
+            for num in lst:
+                traverse(num, depth + 1)
+
+        for num in nestedList:
+            traverse(num, 1)
+
+        total = 0
+        for num, depth in integers_depth:
+            total += num * (max_depth[0] - depth + 1)
+        return total
+
