244b82e1ff3529659cf163be6ccd410b856b02bc
jyztintan <justintanwk2001@gmail.com>
2025-11-30 08:56:50 +0800

    Add new pset

--- Python/make_sum_divisible_p.py
+++ Python/make_sum_divisible_p.py
@@ -1,29 +1,24 @@
 class Solution:
-    def minSubarray(self, nums, p: int) -> int:
+    def minSubarray(self, nums: List[int], p: int) -> int:
         total = sum(nums)
         if total < p:
             return -1
-        if total % p == 0:
+        elif total % p == 0:
             return 0
 
         target = total % p
         best = float('inf')
-        prefix_sum = 0
-        seen = {0: -1}
+        curr = 0
+        seen = {0: -1}  # offset for start of array
 
         for i, num in enumerate(nums):
-            prefix_sum = (prefix_sum + num) % p
-            complement = (prefix_sum - target) % p
-
+            curr = (curr + num) % p
+            complement = (curr - target) % p
             if complement in seen:
                 best = min(best, i - seen[complement])
-            seen[prefix_sum] = i
+            seen[curr] = i
 
+        # not allowed to remove whole array
         if best == len(nums):
-            return -1
+            return - 1
         return best
-
-
-nums = [3, 1, 4, 2]
-print(Solution().minSubarray(nums, 6))
-
