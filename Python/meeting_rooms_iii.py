4e2478829bd00244bf4d22b453312a65749f9106
jyztintan <justintanwk2001@gmail.com>
2025-12-27 10:12:35 +0800

    Add new pset

--- Python/meeting_rooms_iii.py
+++ Python/meeting_rooms_iii.py
@@ -1,28 +1,26 @@
 from heapq import *
 
-
 class Solution:
     def mostBooked(self, n: int, meetings: List[List[int]]) -> int:
         count = [0] * n
 
-        # Keep track of next available timing for a used room
-        used = []
-        unused = list(range(n))
-        heapify(unused)
-        meetings.sort()
+        available = list(range(n))  # sort by room
+        heapify(available)
+        used = []  # sort by end time
 
-        for start, end in meetings:
+        for start, end in sorted(meetings):
+            # vacate rooms that have ended their meetings
             while used and used[0][0] <= start:
                 _, room = heappop(used)
-                heappush(unused, room)
+                heappush(available, room)
 
-            if unused:
-                room = heappop(unused)
-                heappush(used, [end, room])
-
-            else:
-                curr_end, room = heappop(used)
-                heappush(used, [curr_end + end - start, room])
+            if available:  # use available if any
+                room = heappop(available)
+                heappush(used, (end, room))
+            else:  # otherwise delay to next available
+                duration = end - start
+                time, room = heappop(used)
+                heappush(used, (time + duration, room))
 
             count[room] += 1
 
