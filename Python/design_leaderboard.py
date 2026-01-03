657382f7dfd527b6d0cd3c9ffa7a946f31dc0ae7
jyztintan <justintanwk2001@gmail.com>
2026-01-03 13:46:42 +0800

    Add new pset

---
+++ Python/design_leaderboard.py
@@ -0,0 +1,19 @@
+class Leaderboard:
+
+    def __init__(self):
+        self.scores = defaultdict(int)
+
+    def addScore(self, playerId: int, score: int) -> None:
+        self.scores[playerId] += score
+
+    def top(self, K: int) -> int:
+        return sum(sorted(self.scores.values())[-K:])
+
+    def reset(self, playerId: int) -> None:
+        self.scores[playerId] = 0
+
+# Your Leaderboard object will be instantiated and called as such:
+# obj = Leaderboard()
+# obj.addScore(playerId,score)
+# param_2 = obj.top(K)
+# obj.reset(playerId)
