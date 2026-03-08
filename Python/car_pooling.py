9a79a6efd0f737b9266c988e4d4e00537d8e1b75
jyztintan <justintanwk2001@gmail.com>
2026-03-08 15:20:14 +0800

    Add new pset

--- Python/car_pooling.py
+++ Python/car_pooling.py
@@ -1,3 +1,4 @@
+# This only works because we are given that 0 <= from_i < to_i <= 1000
 class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
         events = [0] * 1001
@@ -10,4 +11,20 @@ class Solution:
             curr += events[i]
             if curr > capacity:
                 return False
+        return True
+
+
+class Solution:
+    def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
+        trips.sort(key=lambda x: x[1])
+        passengers = []
+        curr = 0
+        for board, start, end in trips:
+            while passengers and start >= passengers[0][0]:
+                _, alight = heappop(passengers)
+                curr -= alight
+            curr += board
+            if curr > capacity:
+                return False
+            heappush(passengers, (end, board))
         return True
