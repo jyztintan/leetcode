333aa3053d521bfdc1dab90c4008fb770a18a08b
jyztintan <justintanwk2001@gmail.com>
2026-03-27 13:51:09 +0800

    Add new pset

--- Python/matrix_similarity_after_cyclic_shifts.py
+++ Python/matrix_similarity_after_cyclic_shifts.py
@@ -2,7 +2,9 @@ class Solution:
     def areSimilar(self, mat: List[List[int]], k: int) -> bool:
         m, n = len(mat), len(mat[0])
         for row in range(m):
-            delta = k if row % 2 == 0 else -k
+            # Shifting left or right identical check is equivalent
+            # delta = k if row % 2 == 0 else -k
+            delta = k
             for col in range(n):
                 if mat[row][col] != mat[row][(col + delta) % n]:
                     return False
