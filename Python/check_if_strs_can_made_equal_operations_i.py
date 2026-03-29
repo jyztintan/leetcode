a4a5abeaf657b55597d6406c5d194a15fcd8ec0b
jyztintan <justintanwk2001@gmail.com>
2026-03-29 09:20:03 +0800

    Add new pset

---
+++ Python/check_if_strs_can_made_equal_operations_i.py
@@ -0,0 +1,19 @@
+class Solution:
+    def canBeEqual(self, s1: str, s2: str) -> bool:
+        odd = {}
+        even = {}
+        for i, c in enumerate(s1):
+            if i % 2:
+                odd[c] = odd.get(c, 0) + 1
+            else:
+                even[c] = even.get(c, 0) + 1
+        for i, c in enumerate(s2):
+            if i % 2:
+                if odd.get(c, 0) == 0:
+                    return False
+                odd[c] -= 1
+            else:
+                if even.get(c, 0) == 0:
+                    return False
+                even[c] -= 1
+        return True
