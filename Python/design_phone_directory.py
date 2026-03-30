f582c1e53457d75024e2a9b04bdd00b11c22dd66
jyztintan <justintanwk2001@gmail.com>
2026-03-30 10:24:06 +0800

    Add new pset

--- Python/design_phone_directory.py
+++ Python/design_phone_directory.py
@@ -1,3 +1,35 @@
+# Optimised because now the initialisation is also O(1)
+class PhoneDirectory:
+
+    def __init__(self, maxNumbers: int):
+        self.available = 0
+        self.max = maxNumbers
+        self.released = set()
+
+    def get(self) -> int:
+        if self.available == self.max and len(self.released) == 0:
+            return -1
+        if self.available < self.max:
+            num = self.available
+            self.available += 1
+        else:
+            num = self.released.pop()
+        return num
+
+    def check(self, number: int) -> bool:
+        return number >= self.available or number in self.released
+
+    def release(self, number: int) -> None:
+        if number < self.available:
+            self.released.add(number)
+
+
+# Your PhoneDirectory object will be instantiated and called as such:
+# obj = PhoneDirectory(maxNumbers)
+# param_1 = obj.get()
+# param_2 = obj.check(number)
+# obj.release(number)
+
 class PhoneDirectory:
 
     def __init__(self, maxNumbers: int):
