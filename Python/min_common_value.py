6b813a4a6e224c73903708942fe003aa55533471
jyztintan <justintanwk2001@gmail.com>
2026-05-19 13:35:35 +0800

    Add new pset

---
+++ Python/min_common_value.py
@@ -0,0 +1,7 @@
+class Solution:
+    def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
+        nums1 = set(nums1)
+        for num in nums2:
+            if num in nums1:
+                return num
+        return -1
